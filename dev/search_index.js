0c1c9478d0a762d74dcdcdaf6ea063b779b6981c
Documenter.jl <documenter@juliadocs.github.io>
2024-01-29 09:19:57 +0000

    build based on 7e604e8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SchrodingerSolver","category":"page"},{"location":"#SchrodingerSolver","page":"Home","title":"SchrodingerSolver","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SchrodingerSolver.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SchrodingerSolver]","category":"page"}]
+}
